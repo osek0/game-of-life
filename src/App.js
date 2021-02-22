77e48e53d10305bf2bd435cb118e0351ab1d19e5
Oskar Dyngus <dyngusoskar@gmail.com>
2021-02-22 12:42:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState, useRef } from 'react';
+import produce from 'immer';
 
-function App() {
+const numRows = 50;
+const numCols = 50;
+const operations = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [1, 0],
+  [1, 1]
+];
+
+const createZeroesGrid = () => {
+  const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+
+    return rows;
+}
+
+const App = () => {
+
+  const [grid, setGrid] = useState(() => createZeroesGrid());
+  const [started, setStarted] = useState(false);
+
+  const generateGrid = () => {
+    return grid.map((rows, rowIndx) => {
+      return rows.map((col, colIndx) => {
+        return( 
+        <div 
+          key={`${rowIndx}${colIndx}`} 
+          onClick={() => {
+            const newGrid = produce(grid, draft => {
+              draft[rowIndx][colIndx] = grid[rowIndx][colIndx] ? 0 : 1;
+            })
+            return setGrid(newGrid);
+          }} 
+          style={{ 
+            width: '20px', 
+            height: '20px', 
+            backgroundColor: col ? 'black' : undefined,
+            border: 'solid 1px lightgrey'
+        }}>
+        </div>
+        );
+      });
+    });
+  };
+
+  const startedRef = useRef(started);
+  startedRef.current = started;
+
+  const runSimulation = useCallback(() => {
+    if(!startedRef.current) {
+      return;
+    }
+    
+    setGrid(grid => {
+      return produce(grid, draft => {
+        for(let i = 0; i < numRows; i++) {
+          for(let j = 0; j < numCols; j++) {
+            let neighbors = 0;
+
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newJ = j + y;
+              if (newI >=0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+                neighbors += grid[newI][newJ];
+              }
+            });
+
+            if(neighbors < 2 || neighbors > 3) {
+              draft[i][j] = 0;
+            } else if(grid[i][j] === 0 && neighbors === 3) {
+              draft[i][j] = 1;
+            }
+          }
+        }
+      });
+    });
+
+    setTimeout(runSimulation, 100);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button 
+        onClick={() => {
+          setStarted(!started);
+          if(!started) {
+            startedRef.current = true;
+            runSimulation();
+          }
+        }}
+      >
+        {started ? 'stop' : 'start'}
+      </button>
+      <div className="App" style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${numCols}, 20px)`
+      }}>
+        {generateGrid()}
+      </div>
+    </>
   );
 }
 
